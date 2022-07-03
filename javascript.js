bea683b3786d5c83bfe6d87e2912174b901104cc
AndrewLin95 <andrew.lin88@hotmail.com>
2022-07-03 14:02:38 -0400

    added functionality to parse form information and generate new card based on information submitted.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,9 @@ const addBookBtn = document.querySelector('#addBookBtn');
 const submitBtn = document.querySelector('#submitBtn');
 const cancelBtn = document.querySelector('#cancelBtn');
 const form = document.querySelector('#form');
-let aBook = '';
+
+
+
 
 let myLibrary = [
     {author: 'J.K. Rowling', title: 'Harry Potter and the Philosopher\'s Stone', pages: '10', readStatus: false,}
@@ -15,9 +17,40 @@ function Book(author, title, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
-function addBookToLibrary(author, title, pages, readStatus) {
-    
+let i = 0;
+addBookToLibrary();
+
+function addBookToLibrary() {
+    const libraryDiv = document.querySelector('#books');
+    const newCard = document.createElement('div');
+    const newTitle = document.createElement('h4');
+    const newAuthor = document.createElement('div');
+    const newPages = document.createElement('div');
+    const newReadStatus = document.createElement('button');
+
+    libraryDiv.appendChild(newCard);
+    newCard.className = 'card number_' + i;
+
+    newTitle.textContent = myLibrary[i].title;
+    newAuthor.textContent = myLibrary[i].author;
+    newPages.textContent = 'Total Pages: ' + myLibrary[i].pages;
+    newReadStatus.textContent = myLibrary[i].readStatus;
+
+    newCard.appendChild(newTitle);
+    newCard.appendChild(newAuthor);
+    newCard.appendChild(newPages);
+    newCard.appendChild(newReadStatus);
+
+    i++;
 }
+
+function createLibraryCard() {
+
+
+}
+
+
+let aBook = '';
 
 function pullBookInfo(){
     const authorValue = document.querySelector('#author');
@@ -25,10 +58,12 @@ function pullBookInfo(){
     const pagesValue = document.querySelector("#pageNum");
     const readStatusCheck = document.querySelector('#readStatus');
     aBook = new Book(authorValue.value, titleValue.value, pagesValue.value, readStatusCheck.checked);
+    myLibrary.push(aBook);
     console.log(aBook.author);
     console.log(aBook.title);
     console.log(aBook.pages);
     console.log(aBook.readStatus);
+    console.table(myLibrary);
 }
 
 addBookBtn.addEventListener('click', () => {
